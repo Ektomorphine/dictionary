d3d7e3520a3c5df6a37160ef0fdddc4af3c12b59
GladkovM <iglad96@gmail.com>
2017-10-17 19:30:28 +0300

    Create word add form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header-component/header.component';
 import { SidebarComponent } from './sidebar-component/sidebar.component';
+import { WordsComponent } from './words-component/words.component';
+
+const appRoutes: Routes =[
+  { path: 'words', component: WordsComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    WordsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   bootstrap: [AppComponent]
 })
